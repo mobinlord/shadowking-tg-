8b400b3f4b51b0b5786f7da000c0df7c87931b0d
mobinlord <mobinlord29@gmail.com>
2016-05-21 23:15:47 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'teleseed' then -- Put everything you like :)
+  if matches[1]:lower() == 'shadow' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "teleseed" then -- Put everything you like :)
+    if matches[2] == "shadow" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -141,10 +141,10 @@ end
 return {
   patterns = {
     "^[#!/]([Ss]tats)$",
-    "^[#!/]([Ss]tatslist)$",
+    "^[#!/]([Ss]hadow)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (teleseed)",
-	"^[#!/]([Tt]eleseed)"
+    "^[#!/]([Ss]tats) (shadow)",
+	"^[#!/]([Ss]hadow)"
     }, 
   run = run
 }
