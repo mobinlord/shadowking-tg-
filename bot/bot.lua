94df8d68fbe2a3e65053da12f903832c5230c113
mobinlord <mobinlord29@gmail.com>
2016-05-21 15:16:34 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -232,7 +232,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {219339258},--Sudo users
+    sudo_users = {219339258,175636120,198794027},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
